cfcd3f1b4fad00883c0d2523c8e5ecba1d8dd723
Zhaochen Yu <115272494+BitCodingWalkin@users.noreply.github.com>
2024-06-24 22:05:47 +0800

    Update bot_pipeline.py
    Update inspector for more stable generation

--- bot_pipeline.py
+++ bot_pipeline.py
@@ -82,14 +82,16 @@ class Pipeline:
 
         
 class BoT:
-    def __init__(self, user_input,problem_id,api_key=None,model_id=None):
+    def __init__(self, user_input,problem_id,api_key=None,model_id=None,need_check=False):
         self.api_key = api_key
         self.model_id = model_id
         self.pipeline = Pipeline(self.model_id,self.api_key)
         self.user_input = user_input
         # Only for test use, stay tuned for our update
         self.problem_id = problem_id 
-        
+        self.need_check = need_check
+    def update_input(self,new_input):
+        self.user_input = new_input
     def problem_distillation(self):
         print(f'User prompt:{self.user_input}')
         self.distilled_information = self.pipeline.get_respond(meta_distiller_prompt, self.user_input)
@@ -109,22 +111,55 @@ class BoT:
         problem_id_list = [0,1,2]
         self.instantiation_instruct = """
 You are an expert in problem analysis and can apply previous problem-solving approaches to new issues. The user will provide a specific task description and a thought template. Your goal is to analyze the user's task and generate a specific solution based on the thought template. If the instantiated solution involves Python code, only provide the code and let the compiler handle it. If the solution does not involve code, provide a final answer that is easy to extract from the text.
+# It should be noted that all the python code should be within one code block, the answer should not include more than one code block!
         """
         
         self.formated_input = f"""
 Distilled information:
 {self.distilled_information}
+User Input:
+{self.user_input}
 Thought template:
 {self.thought_template}
 
 Instantiated Solution:
 Please analyze the above user task description and thought template, and generate a specific, detailed solution. If the solution involves Python code, only provide the code. If not, provide a clear and extractable final answer.        
         """
-
+        self.inspector_prompt = """
+You are an excellent python programming master who are proficient in analyzing and editing python code, and you are also good at understanding the real-world problem. Your task is:
+1. Analyze the given python code
+2. Edit the input code to make sure the edited code is correct and could run and solve the problem correctly.  
+Your respond should follow the format below:
+```python
+## Edited code here
+```
+        """
         self.result = self.pipeline.get_respond(self.instantiation_instruct,self.formated_input)
         print(f'Instantiated reasoning result: {self.result}')
         if self.problem_id in problem_id_list:
             self.final_result, code_str = extract_and_execute_code(self.result)
+            if self.need_check:
+                self.count = 0
+                self.inter_input = f"""
+                User_input:{self.user_input}
+                {code_str}
+                {self.final_result}
+                """
+                self.inter_result = self.final_result
+                while(('An error occurred' in self.inter_result) or (self.inter_result == '') or (self.inter_result == 'None')):
+                    print('The code cannot be executed correctly, here we continue the edit phase:',self.inter_result)
+                    self.inter_input = self.pipeline.get_respond(self.inspector_prompt,self.inter_input)
+                    print(self.inter_input)
+                    self.inter_result, inter_code_str = extract_and_execute_code(self.inter_input)
+                    self.inter_input = f"""
+                User_input:{self.user_input}
+                {inter_code_str}
+                The result of code execution: {self.inter_result}
+                """
+                    self.count = self.count + 1
+                    if self.count > 3:
+                        break
+                self.final_result = self.inter_result 
             print(f'The result of code execution: {self.final_result}')
         else:
             self.final_result = self.result 
